08dc1a16cf3af658672c09f4cdee1208971e9e5a
mpoxson <mpoxson@gmail.com>
2023-12-05 23:29:24 -0500

    Day1

--- RN-MyToDoApp/App.js
+++ RN-MyToDoApp/App.js
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import React, { useState } from "react";
 
@@ -6,6 +13,28 @@ import React, { useState } from "react";
 import task from "./components/Task";
 
 export default function App() {
+  //input area
+  const [task, setTask] = useState();
+
+  //to store all tasks
+  const [taskItems, setTaskItems] = useState([]);
+
+  //event listener logic
+  const handleAddTask = () => {
+    keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
+  //event listener logic for deleting task
+  const completeTask = () => {
+    let copyItems = [...taskItems];
+    //remove the specific task as selected by user
+    copyItems.splice(index, 1);
+    //update the original array by rewriting it with the copied array
+    setTaskItems(copyItems);
+  };
+
   return (
     <View style={styles.container}>
       {/* List all todo items */}
@@ -14,11 +43,26 @@ export default function App() {
 
         {/* container/wrapper for all tasks to be rendered */}
         <View style={styles.items}>
-          <Task text={"this is task 1"} />
-          <Task text={"this is task 2"} />
-          <Task text={"this is task 3"} />
+          {/* <Task text={"this is task 1"} /> */}
         </View>
       </View>
+      {/* User input for todo tasks */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Add your to do item here"}
+          value="task"
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.andWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -27,5 +71,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
